e18549bdb933bfb4374fc7875918a492fc3f1b03
Cleber Severo <clebersevero1998@gmail.com>
2025-11-09 19:35:02 -0300

    fix: remove unused content

--- src/screens/SignOut.tsx
+++ src/screens/SignOut.tsx
@@ -1,9 +1,7 @@
 import { useAuth } from '@hooks/useAuth';
-import { useNavigation } from '@react-navigation/native';
 import { useEffect } from 'react';
 
 export function SignOutPage() {
-  const navigation = useNavigation();
   const { signOut } = useAuth();
 
   useEffect(() => {
